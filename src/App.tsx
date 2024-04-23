e2268b80a1c03d2f3d4a8595a25db461b09cefde
DouglasB <douglas_borges09@hmail.com>
2024-04-23 08:05:33 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1>Pizzaria</h1>
+      <Button>enviar</Button>
+    </div>
+  );
+}
